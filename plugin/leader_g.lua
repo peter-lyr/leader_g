231de13ff544cceba6404d4d7e58bb9d1cd0ccbe
peter-lyr <llydrp.ldp@gmail.com>
2024-09-20 22:53:46 +0800

    F.git_reset_buffer()

--- plugin/leader_g.lua
+++ plugin/leader_g.lua
@@ -3,4 +3,6 @@ local F = require 'f'
 require 'which-key'.register {
   ['<leader>g'] = { name = 'git', },
   ['<leader>ga'] = { function() F.git_add_commit_push() end, 'git_add_commit_push', mode = { 'n', 'v', }, silent = true, },
+  ['<leader>gr'] = { function() F.reset_hunk() end, 'reset_hunk', mode = { 'n', 'v', }, silent = true, },
+  ['<leader>g<localleader>r'] = { function() F.git_reset_buffer() end, 'git_reset_buffer', mode = { 'n', 'v', }, silent = true, },
 }
