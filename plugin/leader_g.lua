4be3b21735b2826226f3c09cfd790801aaba1b24
peter-lyr <llydrp.ldp@gmail.com>
2024-09-21 00:38:38 +0800

    M.git_lazy

--- plugin/leader_g.lua
+++ plugin/leader_g.lua
@@ -5,4 +5,5 @@ require 'which-key'.register {
   ['<leader>ga'] = { function() F.git_add_commit_push() end, 'git_add_commit_push', mode = { 'n', 'v', }, silent = true, },
   ['<leader>gr'] = { function() F.reset_hunk() end, 'reset_hunk', mode = { 'n', 'v', }, silent = true, },
   ['<leader>g<localleader>r'] = { function() F.git_reset_buffer() end, 'git_reset_buffer', mode = { 'n', 'v', }, silent = true, },
+  ['<leader>gl'] = { function() F.git_lazy() end, 'git_lazy', mode = { 'n', 'v', }, silent = true, },
 }
