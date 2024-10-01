6112580be88ea5dd83b8bb6fa947f86fcbac2ea8
peter-lyr <llydrp.ldp@gmail.com>
2024-10-01 08:25:03 +0800

    testing...

--- plugin/leader_g.lua
+++ plugin/leader_g.lua
@@ -11,8 +11,11 @@ require 'which-key'.register {
   ['<leader>gP'] = { function() F.git_pull_recursive 'clone' end, 'git_pull_recursive try clone', mode = { 'n', 'v', }, silent = true, },
   ['<leader>g<localleader>p'] = { function() F.git_pull() end, 'git_pull', mode = { 'n', 'v', }, silent = true, },
 
-  ['<leader>ga'] = { function() F.git_push_recursive() end, 'git_push_recursive', mode = { 'n', 'v', }, silent = true, },
-  ['<leader>g<localleader>a'] = { function() F.git_add_commit_push() end, 'git_add_commit_push', mode = { 'n', 'v', }, silent = true, },
+  ['<leader>ga'] = { function() F.git_add_commit_push_recursive() end, 'git add commit push recursive', mode = { 'n', 'v', }, silent = true, },
+  ['<leader>g<localleader>a'] = { function() F.git_add_commit_push() end, 'git add commit push', mode = { 'n', 'v', }, silent = true, },
+
+  ['<leader>gc'] = { function() F.git_commit_push_recursive() end, 'git commit push recursive', mode = { 'n', 'v', }, silent = true, },
+  ['<leader>gP'] = { function() F.git_push_recursive() end, 'git push recursive', mode = { 'n', 'v', }, silent = true, },
 
   ['<leader>gr'] = { function() F.reset_hunk() end, 'reset_hunk', mode = { 'n', 'v', }, silent = true, },
   ['<leader>g<localleader>r'] = { function() F.git_reset_buffer() end, 'git_reset_buffer', mode = { 'n', 'v', }, silent = true, },
