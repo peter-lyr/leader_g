338ba3c82ca1c93484161a07ec28edc84001fb5c
peter-lyr <llydrp.ldp@gmail.com>
2024-10-01 08:25:31 +0800

    <leader>g<leader>d require 'gitsigns'.diffthis '~1'

--- plugin/leader_g.lua
+++ plugin/leader_g.lua
@@ -2,13 +2,14 @@ local F = require 'f'
 
 require 'which-key'.register {
   ['<leader>g'] = { name = 'git', },
+  ['<leader>g<leader>'] = { name = 'git.more', },
   ['<leader>g<localleader>'] = { name = 'git.more', },
 
   ['<leader>g<localleader>sp'] = { function() F.git_create_submodule_public() end, 'git_create_submodule_public', mode = { 'n', 'v', }, silent = true, },
   ['<leader>g<localleader>sv'] = { function() F.git_create_submodule_private() end, 'git_create_submodule_private', mode = { 'n', 'v', }, silent = true, },
 
   ['<leader>gp'] = { function() F.git_pull_recursive() end, 'git_pull_recursive', mode = { 'n', 'v', }, silent = true, },
-  ['<leader>gP'] = { function() F.git_pull_recursive 'clone' end, 'git_pull_recursive try clone', mode = { 'n', 'v', }, silent = true, },
+  ['<leader>g<leader>p'] = { function() F.git_pull_recursive 'clone' end, 'git_pull_recursive try clone', mode = { 'n', 'v', }, silent = true, },
   ['<leader>g<localleader>p'] = { function() F.git_pull() end, 'git_pull', mode = { 'n', 'v', }, silent = true, },
 
   ['<leader>ga'] = { function() F.git_add_commit_push_recursive() end, 'git add commit push recursive', mode = { 'n', 'v', }, silent = true, },
@@ -18,13 +19,13 @@ require 'which-key'.register {
   ['<leader>gP'] = { function() F.git_push_recursive() end, 'git push recursive', mode = { 'n', 'v', }, silent = true, },
 
   ['<leader>gr'] = { function() F.reset_hunk() end, 'reset_hunk', mode = { 'n', 'v', }, silent = true, },
-  ['<leader>g<localleader>r'] = { function() F.git_reset_buffer() end, 'git_reset_buffer', mode = { 'n', 'v', }, silent = true, },
+  ['<leader>g<leader>r'] = { function() F.git_reset_buffer() end, 'git_reset_buffer', mode = { 'n', 'v', }, silent = true, },
 
   ['<leader>gs'] = { function() F.stage_hunk() end, 'stage_hunk', mode = { 'n', 'v', }, silent = true, },
   ['<leader>gu'] = { function() F.undo_stage_hunk() end, 'undo_stage_hunk', mode = { 'n', 'v', }, silent = true, },
 
   ['<leader>gd'] = { function() require 'gitsigns'.diffthis() end, 'undo_stage_hunk', mode = { 'n', 'v', }, silent = true, },
-  ['<leader>g<localleader>d'] = { function() require 'gitsigns'.diffthis('~1') end, 'undo_stage_hunk', mode = { 'n', 'v', }, silent = true, },
+  ['<leader>g<leader>d'] = { function() require 'gitsigns'.diffthis '~1' end, 'undo_stage_hunk', mode = { 'n', 'v', }, silent = true, },
 
   ['<leader>gl'] = { function() F.git_lazy() end, 'git_lazy', mode = { 'n', 'v', }, silent = true, },
 }
