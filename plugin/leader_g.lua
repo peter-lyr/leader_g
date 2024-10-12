ef66055dea35a1aa91ddf653c68a7699f9dbfe5d
peter-lyr <llydrp.ldp@gmail.com>
2024-10-12 19:28:01 +0800

    git.commit_p.recur regh

--- plugin/leader_g.lua
+++ plugin/leader_g.lua
@@ -1,55 +1,55 @@
 local F = require 'f'
 
 require 'which-key'.register {
-  ['<leader>ga'] = { function() F.git_add_commit_push_recursive() end, 'git.addcommitpush recursive', mode = { 'n', 'v', }, silent = true, },
+  ['<leader>ga'] = { function() F.git_add_commit_push_recursive() end, 'git.add_c_p recur', mode = { 'n', 'v', }, silent = true, },
 
-  ['<leader>g<leader>ap'] = { function() F.git_push_recursive() end, 'git.push recursive', mode = { 'n', 'v', }, silent = true, },
+  ['<leader>g<leader>ap'] = { function() F.git_push_recursive() end, 'git.push recur', mode = { 'n', 'v', }, silent = true, },
 
-  ['<leader>g<leader>a'] = { name = 'git.addcommitpush', },
+  ['<leader>g<leader>a'] = { name = 'git.add_c_p', },
 
-  ['<leader>g<leader>ah'] = { function() F.git_add_commit_push_recursive_quick 'regh' end, 'git.addcommitpush.recursive regh', mode = { 'n', 'v', }, silent = true, },
-  ['<leader>g<leader>aj'] = { function() F.git_add_commit_push_recursive_quick 'reghj' end, 'git.addcommitpush.recursive reghj', mode = { 'n', 'v', }, silent = true, },
-  ['<leader>g<leader>ak'] = { function() F.git_add_commit_push_recursive_quick 'reghjk' end, 'git.addcommitpush.recursive reghjk', mode = { 'n', 'v', }, silent = true, },
-  ['<leader>g<leader>al'] = { function() F.git_add_commit_push_recursive_quick 'reghjkl' end, 'git.addcommitpush.recursive reghjkl', mode = { 'n', 'v', }, silent = true, },
+  ['<leader>g<leader>ah'] = { function() F.git_add_commit_push_recursive_quick 'regh' end, 'git.add_c_p.recur regh', mode = { 'n', 'v', }, silent = true, },
+  ['<leader>g<leader>aj'] = { function() F.git_add_commit_push_recursive_quick 'reghj' end, 'git.add_c_p.recur reghj', mode = { 'n', 'v', }, silent = true, },
+  ['<leader>g<leader>ak'] = { function() F.git_add_commit_push_recursive_quick 'reghjk' end, 'git.add_c_p.recur reghjk', mode = { 'n', 'v', }, silent = true, },
+  ['<leader>g<leader>al'] = { function() F.git_add_commit_push_recursive_quick 'reghjkl' end, 'git.add_c_p.recur reghjkl', mode = { 'n', 'v', }, silent = true, },
 
-  ['<leader>g<leader>ae'] = { function() F.git_add_commit_push_recursive_quick 'cword' end, 'git.addcommitpush.recursive cword', mode = { 'n', 'v', }, silent = true, },
-  ['<leader>g<leader>ar'] = { function() F.git_add_commit_push_recursive_quick 'cWORD' end, 'git.addcommitpush.recursive cWORD', mode = { 'n', 'v', }, silent = true, },
+  ['<leader>g<leader>ae'] = { function() F.git_add_commit_push_recursive_quick 'cword' end, 'git.add_c_p.recur cword', mode = { 'n', 'v', }, silent = true, },
+  ['<leader>g<leader>ar'] = { function() F.git_add_commit_push_recursive_quick 'cWORD' end, 'git.add_c_p.recur cWORD', mode = { 'n', 'v', }, silent = true, },
 
-  ['<leader>g<leader>ay'] = { function() F.git_add_commit_push_recursive_quick 'yanked' end, 'git.addcommitpush.recursive yanked', mode = { 'n', 'v', }, silent = true, },
+  ['<leader>g<leader>ay'] = { function() F.git_add_commit_push_recursive_quick 'yanked' end, 'git.add_c_p.recur yanked', mode = { 'n', 'v', }, silent = true, },
 
-  ['<leader>g<leader>ai'] = { function() F.git_add_commit_push_recursive_quick 'line' end, 'git.addcommitpush.recursive line', mode = { 'n', 'v', }, silent = true, },
+  ['<leader>g<leader>ai'] = { function() F.git_add_commit_push_recursive_quick 'line' end, 'git.add_c_p.recur line', mode = { 'n', 'v', }, silent = true, },
 
-  ['<leader>g<leader>at'] = { function() F.git_add_commit_push_recursive_quick 'tail' end, 'git.addcommitpush.recursive tail', mode = { 'n', 'v', }, silent = true, },
+  ['<leader>g<leader>at'] = { function() F.git_add_commit_push_recursive_quick 'tail' end, 'git.add_c_p.recur tail', mode = { 'n', 'v', }, silent = true, },
 
-  ['<leader>g<leader>a<localleader>'] = { name = 'git.addcommitpush.recursive.treesitter', },
-  ['<leader>g<leader>a<localleader>a'] = { function() F.git_add_commit_push_recursive_quick 'treesitter1' end, 'git.addcommitpush.recursive treesitter1', mode = { 'n', 'v', }, },
-  ['<leader>g<leader>a<localleader>b'] = { function() F.git_add_commit_push_recursive_quick 'treesitter2' end, 'git.addcommitpush.recursive treesitter2', mode = { 'n', 'v', }, },
-  ['<leader>g<leader>a<localleader>c'] = { function() F.git_add_commit_push_recursive_quick 'treesitter3' end, 'git.addcommitpush.recursive treesitter3', mode = { 'n', 'v', }, },
+  ['<leader>g<leader>a<localleader>'] = { name = 'git.add_c_p.recur.treesitter', },
+  ['<leader>g<leader>a<localleader>a'] = { function() F.git_add_commit_push_recursive_quick 'treesitter1' end, 'git.add_c_p.recur treesitter1', mode = { 'n', 'v', }, },
+  ['<leader>g<leader>a<localleader>b'] = { function() F.git_add_commit_push_recursive_quick 'treesitter2' end, 'git.add_c_p.recur treesitter2', mode = { 'n', 'v', }, },
+  ['<leader>g<leader>a<localleader>c'] = { function() F.git_add_commit_push_recursive_quick 'treesitter3' end, 'git.add_c_p.recur treesitter3', mode = { 'n', 'v', }, },
 }
 
 require 'which-key'.register {
-  ['<leader>gc'] = { function() F.git_commit_push_recursive() end, 'git.commitpush recursive', mode = { 'n', 'v', }, silent = true, },
+  ['<leader>gc'] = { function() F.git_commit_push_recursive() end, 'git.commit_p recur', mode = { 'n', 'v', }, silent = true, },
 
-  ['<leader>g<leader>c'] = { name = 'git.commitpush.recursive', },
+  ['<leader>g<leader>c'] = { name = 'git.commit_p.recur', },
 
-  ['<leader>g<leader>ch'] = { function() F.git_commit_push_recursive_quick 'regh' end, 'git.commitpush.recursive regh', mode = { 'n', 'v', }, silent = true, },
-  ['<leader>g<leader>cj'] = { function() F.git_commit_push_recursive_quick 'reghj' end, 'git.commitpush.recursive reghj', mode = { 'n', 'v', }, silent = true, },
-  ['<leader>g<leader>ck'] = { function() F.git_commit_push_recursive_quick 'reghjk' end, 'git.commitpush.recursive reghjk', mode = { 'n', 'v', }, silent = true, },
-  ['<leader>g<leader>cl'] = { function() F.git_commit_push_recursive_quick 'reghjkl' end, 'git.commitpush.recursive reghjkl', mode = { 'n', 'v', }, silent = true, },
+  ['<leader>g<leader>ch'] = { function() F.git_commit_push_recursive_quick 'regh' end, 'git.commit_p.recur regh', mode = { 'n', 'v', }, silent = true, },
+  ['<leader>g<leader>cj'] = { function() F.git_commit_push_recursive_quick 'reghj' end, 'git.commit_p.recur reghj', mode = { 'n', 'v', }, silent = true, },
+  ['<leader>g<leader>ck'] = { function() F.git_commit_push_recursive_quick 'reghjk' end, 'git.commit_p.recur reghjk', mode = { 'n', 'v', }, silent = true, },
+  ['<leader>g<leader>cl'] = { function() F.git_commit_push_recursive_quick 'reghjkl' end, 'git.commit_p.recur reghjkl', mode = { 'n', 'v', }, silent = true, },
 
-  ['<leader>g<leader>ce'] = { function() F.git_commit_push_recursive_quick 'cword' end, 'git.commitpush.recursive cword', mode = { 'n', 'v', }, silent = true, },
-  ['<leader>g<leader>cr'] = { function() F.git_commit_push_recursive_quick 'cWORD' end, 'git.commitpush.recursive cWORD', mode = { 'n', 'v', }, silent = true, },
+  ['<leader>g<leader>ce'] = { function() F.git_commit_push_recursive_quick 'cword' end, 'git.commit_p.recur cword', mode = { 'n', 'v', }, silent = true, },
+  ['<leader>g<leader>cr'] = { function() F.git_commit_push_recursive_quick 'cWORD' end, 'git.commit_p.recur cWORD', mode = { 'n', 'v', }, silent = true, },
 
-  ['<leader>g<leader>cy'] = { function() F.git_commit_push_recursive_quick 'yanked' end, 'git.commitpush.recursive yanked', mode = { 'n', 'v', }, silent = true, },
+  ['<leader>g<leader>cy'] = { function() F.git_commit_push_recursive_quick 'yanked' end, 'git.commit_p.recur yanked', mode = { 'n', 'v', }, silent = true, },
 
-  ['<leader>g<leader>ci'] = { function() F.git_commit_push_recursive_quick 'line' end, 'git.commitpush.recursive line', mode = { 'n', 'v', }, silent = true, },
+  ['<leader>g<leader>ci'] = { function() F.git_commit_push_recursive_quick 'line' end, 'git.commit_p.recur line', mode = { 'n', 'v', }, silent = true, },
 
-  ['<leader>g<leader>ct'] = { function() F.git_commit_push_recursive_quick 'tail' end, 'git.commitpush.recursive tail', mode = { 'n', 'v', }, silent = true, },
+  ['<leader>g<leader>ct'] = { function() F.git_commit_push_recursive_quick 'tail' end, 'git.commit_p.recur tail', mode = { 'n', 'v', }, silent = true, },
 
-  ['<leader>g<leader>c<localleader>'] = { name = 'git.commitpush.recursive.treesitter', },
-  ['<leader>g<leader>c<localleader>a'] = { function() F.git_commit_push_recursive_quick 'treesitter1' end, 'git.commitpush.recursive treesitter1', mode = { 'n', 'v', }, },
-  ['<leader>g<leader>c<localleader>b'] = { function() F.git_commit_push_recursive_quick 'treesitter2' end, 'git.commitpush.recursive treesitter2', mode = { 'n', 'v', }, },
-  ['<leader>g<leader>c<localleader>c'] = { function() F.git_commit_push_recursive_quick 'treesitter3' end, 'git.commitpush.recursive treesitter3', mode = { 'n', 'v', }, },
+  ['<leader>g<leader>c<localleader>'] = { name = 'git.commit_p.recur.treesitter', },
+  ['<leader>g<leader>c<localleader>a'] = { function() F.git_commit_push_recursive_quick 'treesitter1' end, 'git.commit_p.recur treesitter1', mode = { 'n', 'v', }, },
+  ['<leader>g<leader>c<localleader>b'] = { function() F.git_commit_push_recursive_quick 'treesitter2' end, 'git.commit_p.recur treesitter2', mode = { 'n', 'v', }, },
+  ['<leader>g<leader>c<localleader>c'] = { function() F.git_commit_push_recursive_quick 'treesitter3' end, 'git.commit_p.recur treesitter3', mode = { 'n', 'v', }, },
 }
 
 require 'which-key'.register {
