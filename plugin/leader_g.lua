31fe0a518737b4f20b1ed7a3a8bf8e8ff2dcc640
peter-lyr <llydrp.ldp@gmail.com>
2024-11-07 23:45:58 +0800

    git_pull_recursive checkout try clone

--- plugin/leader_g.lua
+++ plugin/leader_g.lua
@@ -70,7 +70,7 @@ require 'which-key'.register {
   ['<leader>g<leader>p'] = { name = 'git.pull', },
 
   ['<leader>g<leader>pj'] = { function() F.git_pull_recursive 'clone' end, 'git_pull_recursive try clone', mode = { 'n', 'v', }, silent = true, },
-  ['<leader>g<leader>pf'] = { function() F.git_pull_recursive('clone', 'checkout') end, 'git_pull_recursive try clone', mode = { 'n', 'v', }, silent = true, },
+  ['<leader>g<leader>pf'] = { function() F.git_pull_recursive('clone', 'checkout') end, 'git_pull_recursive checkout try clone', mode = { 'n', 'v', }, silent = true, },
   ['<leader>g<leader>pc'] = { function() F.git_pull() end, 'git_pull', mode = { 'n', 'v', }, silent = true, },
 }
 
