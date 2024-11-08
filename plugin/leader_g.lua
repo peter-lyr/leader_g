c29e888a24bc5b340b9e4c1991ee6791763235c3
peter-lyr <llydrp.ldp@gmail.com>
2024-11-08 09:41:42 +0800

    <leader>g<leader>sd F.git_show_commits()

--- plugin/leader_g.lua
+++ plugin/leader_g.lua
@@ -62,7 +62,8 @@ require 'which-key'.register {
   ['<leader>g<leader>sp'] = { function() F.git_create_submodule_public() end, 'git_create_submodule_public', mode = { 'n', 'v', }, silent = true, },
   ['<leader>g<leader>sv'] = { function() F.git_create_submodule_private() end, 'git_create_submodule_private', mode = { 'n', 'v', }, silent = true, },
   ['<leader>g<leader>ss'] = { function() F.git_status_recursive() end, 'git_status_recursive', mode = { 'n', 'v', }, silent = true, },
-  ['<leader>g<leader>sc'] = { function() F.git_show_commits() end, 'git_status_recursive', mode = { 'n', 'v', }, silent = true, },
+  ['<leader>g<leader>sc'] = { function() F.git_show_commits() end, 'git_show_commits', mode = { 'n', 'v', }, silent = true, },
+  ['<leader>g<leader>sd'] = { function() F.git_show_commits() end, 'git_show_commits', mode = { 'n', 'v', }, silent = true, },
 }
 
 require 'which-key'.register {
